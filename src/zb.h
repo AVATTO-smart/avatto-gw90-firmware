c2d4e23889cb2378f720de8012bd907966771cbc
AVATTO <leo.yeung@miuctech.com>
2026-03-14 12:31:07 +0800

    Delete src directory

--- src/zb.h
+++
@@ -1,8 +0,0 @@
-void clearS2Buffer();
-void getZbVer();
-void zbCheck();
-void zbLedToggle();
-void checkFwHex(const char *tempFile);
-void runFlash();
-bool programFlashFromFile(const char *filePath);
-void zbInit();

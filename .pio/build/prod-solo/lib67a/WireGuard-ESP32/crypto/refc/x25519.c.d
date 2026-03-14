6a0b7649ffe238c19c8ba02ab7f4433018e9d2b6
AVATTO <leo.yeung@miuctech.com>
2026-03-14 12:30:36 +0800

    Delete .pio directory

--- .pio/build/prod-solo/lib67a/WireGuard-ESP32/crypto/refc/x25519.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/prod-solo/lib67a/WireGuard-ESP32/crypto/refc/x25519.c.o: \
- lib/WireGuard-ESP32/src/crypto/refc/x25519.c \
- lib/WireGuard-ESP32/src/crypto/refc/x25519.h
--- .pio/build/prod-solo/libe6c/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/prod-solo/libe6c/WebServer/detail/mimetable.cpp.o: \
- C:/Users/leifa/.platformio/packages/framework-arduino-solo1/libraries/WebServer/src/detail/mimetable.cpp \
- C:/Users/leifa/.platformio/packages/framework-arduino-solo1/libraries/WebServer/src/detail/mimetable.h \
- C:/Users/leifa/.platformio/packages/framework-arduino-solo1/cores/esp32/pgmspace.h

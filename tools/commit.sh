b79e9cc0b1e86235e075c10dd0dce851c7e761eb
AVATTO <leo.yeung@miuctech.com>
2026-03-14 12:31:30 +0800

    Delete tools directory

--- tools/commit.sh
+++
@@ -1,6 +0,0 @@
-git add -A 
-git commit -F ../commit.md
-version=$(cat version)
-git tag "v$version"
-git push
-git push origin "v$version"
--- tools/webfilesbuilder/buildWeb.bat
+++
@@ -1,7 +0,0 @@
-echo off
-:build
-node gulp.js
-Title "AVATTO-GW90-Ti webfiles builder"
-echo Press any key for build again or close window
-pause
-goto build
